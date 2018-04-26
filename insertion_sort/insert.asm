99f9b55426602844d8c46860d99e7087bcbb44d9
eelvi <drv331@gmail.com>
2018-04-26 22:46:53 +0300

    comments

--- insertion_sort/insert.asm
+++ insertion_sort/insert.asm
@@ -1,4 +1,7 @@
 
+; github/eelvi
+; insertion sort
+
 section .text:
 
 global insertion_sort
@@ -11,42 +14,42 @@ insertion_sort:
     push ebp; stack base
     mov ebp, esp ;make ebp point to this point in the stack
 
-    ; first argument is 6*4 = ebp+ 0x18 
+; first argument is 6*4 = ebp+ 0x18 
 %define INS_DATA [ebp+0x14]
 %define INS_DATALEN [ebp+0x18]
 .begin:
     mov edx, INS_DATA  ; our array base pointer not gonna change
     mov ecx, INS_DATALEN ; length of array
-    shl ecx, 2 ; ecx*=4
+    shl ecx, 2 ; ecx*=4, the size in bytes of the array
     jz .fail
     test edx,edx ;null pointer check
     jz .fail
 
     xor eax,eax ; idx = 0
-    xor ebx,ebx ; second index
+    xor ebx,ebx ; second index = 0
     .loop:
         add ebx, 0x4 ;
         cmp ebx, ecx;
         je .success
         mov esi, [edx+ebx]
-        cmp [edx+eax], esi
+        cmp [edx+eax], esi 
+        ; jump if first operand is greater than the second
         jg .need_correction
         mov eax, ebx
         jmp .loop
    
     ; find position to insert
     .need_correction:
-        ;mov edi, eax;
-        ; above is a redundancy
         mov esi, [edx+ebx]; this is the source int we're trying to find where it fits
         mov edi, ebx ; holds next index (moved to)
         .find_loop:
-            ; comparing pointers, if edi is below the bondary of our array then swap
-            ; because it is the lowest value in the array
+            ; array[i] = array[i-1]
             mov ebp, [edx+eax]
             mov [edx+edi],ebp
-            ; to make space for the last inserted value
             test eax, eax
+            ; comparing pointers, if edi is below the boundary of our array then swap
+            ; because it is the lowest value in the array
+            ; here's we're jumping if the overflow flag is on
             jo .do_swap;
             ; if the value we're holding [edx+ebx] is still less than
             ; the value we're looking at which is behind it, then keep 
--- insertion_sort/x86_64/insert.asm
+++ insertion_sort/x86_64/insert.asm
@@ -1,9 +1,14 @@
 
+; github/eelvi
+; see the x86 version for more comments, this is a slightly modified version of
+; it to use 64bit integers
+
+
 section .text:
 
 global insertion_sort
 
-; insertion_sort, c_decl, (int *data, int len)
+; insertion_sort, SYSTEM V 64 call convention(i guess), (int *data, int len)
 insertion_sort:
     push rbx;
     push rdi;
@@ -11,12 +16,11 @@ insertion_sort:
     push rbp; stack base
     mov rbp, rsp ;make ebp point to this point in the stack
 
-    ; first argument is 6*4 = ebp+ 0x18 
 %define INS_DATA rdi
 %define INS_DATALEN rsi
 .begin:
-    mov rdx, INS_DATA  ; our array base pointer not gonna change
-    mov rcx, INS_DATALEN ; length of array
+    mov rdx, INS_DATA  
+    mov rcx, INS_DATALEN 
     shl rcx, 3 ; ecx*=8
     jz .fail
     test rdx,rdx ;null pointer check
@@ -36,29 +40,19 @@ insertion_sort:
    
     ; find position to insert
     .need_correction:
-        ;mov edi, eax;
-        ; above is a redundancy
         mov rsi, [rdx+rbx]; this is the source int we're trying to find where it fits
         mov rdi, rbx ; holds next index (moved to)
         .find_loop:
-            ; comparing pointers, if edi is below the bondary of our array then swap
-            ; because it is the lowest value in the array
             mov rbp, [rdx+rax]
             mov [rdx+rdi],rbp
-            ; to make space for the last inserted value
             test rax, rax
             jo .do_swap;
-            ; if the value we're holding [edx+ebx] is still less than
-            ; the value we're looking at which is behind it, then keep 
-            ; looking for previous values until we find one or we reach the beginning of the array
-            ;esi is being used as a temporary register
             sub rax, 0x8 
             sub rdi, 0x8 
             cmp rsi,[rdx+rax]
-            jl .find_loop ; if it's larger then it will fall through to the swap part
+            jl .find_loop 
 
 
-        ; now esi is holding the value that we're trying to find where it fits
         .do_swap:
             mov [rdx+rdi], rsi
             mov rax, rbx
